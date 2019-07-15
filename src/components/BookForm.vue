414d9abf848a1a6f6e6f7458da7174769526339f
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-07-15 12:20:12 +0300

    Show How to create a book form Modele complete

---
+++ src/components/BookForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <form class="" action="#" method="post" @submit="bookSubmit(bookTitle, bookAuthor)">
+        <input type="text" name="title" v-model="bookTitle" value="" placeholder="Book Title">
+        <input type="text" name="author" v-model="bookAuthor" value="" placeholder="Book Author">
+        <button type="submit" name="button">Add Book</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'BookForm',
+    props: ['books'],
+    data() {
+      return {
+        bookTitle: '',
+        bookAuthor: ''
+      }
+    },
+    methods: {
+      bookSubmit(bookTitle, bookAuthor) {
+        this.$emit('addBook', bookTitle, bookAuthor);
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
